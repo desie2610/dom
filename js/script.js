1335ca6f96b482e396ea2085edbd9e0321d7eb59
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-07-15 17:48:47 +0300

    ffff

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// const titleEl = document.querySelector(".title")
+// console.log(titleEl);
+
+
+// const linlEl = document.querySelector('a')
+
+// const linklEl = document.querySelectorAll('a')
+
+// console.log(linlkEl);
+
+// const listEl = document.querySelector("ul")
+// console.log(listEl.children);
