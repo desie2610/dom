fe775d9afc4055e04e2c8c413744806b4b81d02b
desie2610 <168661973+desie2610@users.noreply.github.com>
2025-07-17 17:55:22 +0300

    ghghghg

--- js/script.js
+++ js/script.js
@@ -10,3 +10,25 @@
 
 // const listEl = document.querySelector("ul")
 // console.log(listEl.children);
+
+
+
+// Отримайте доступ до списку
+// Додайте жовтий колір фону 
+// Додайте клас "site-nav__link" до першого посилання
+// Збільшіть розмір шрифта посилання з атрибутом href="https://"
+
+
+// const listEl = document.querySelector('.list')
+// console.log(listEl);
+
+// listEl.style.backgroundcolor = "yellow"
+
+const friendName = 
+['anton', 'daria', 'timofii','david','timur', 'katerina', 'oleksandr']
+
+const itemsEl = document.querySelector('.item')
+const itemEl = document.querySelectorAll('.item')
+console.log(itemsEl);
+
+console.log(itemEl);
